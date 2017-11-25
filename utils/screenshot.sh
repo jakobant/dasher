f4aca97fc7797086145a2f6245b412a6cc48fc39
Jakob Antonsson <jakobant@gmail.com>
2017-11-25 16:22:18 +0000

    Fix screenshot for windows

--- utils/screenshot.sh
+++ utils/screenshot.sh
@@ -5,4 +5,18 @@ sleep $WAIT
 export PRE=${2:-temp}
 mkdir -p $HOME/screenshots/$PRE
 export DISPLAY=:0
-/usr/bin/scrot -e '/usr/bin/mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" $f ; mv $f `echo \$HOME`/screenshots/`echo \$PRE`'
+export DD=`date +%Y-%m-%d-%H%M%S`
+if [ -f /etc/fedora-release ]
+then
+	gnome-screenshot -f ${DD}_fedora.png
+	mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" ${DD}_fedora.png
+	mv ${DD}_fedora.png $HOME/screenshots/$PRE
+elif [ -f /etc/redat-release ]
+then
+	gnome-screenshot -f ${DD}_redhat.png
+	mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" ${DD}_redhat.png
+	mv ${DD}_redhat.png $HOME/screenshots/$PRE
+else
+	scrot -e 'mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" $f ; mv $f `echo \$HOME`/screenshots/`echo \$PRE`'
+fi
+
