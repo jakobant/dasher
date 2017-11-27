bae82ed51751f0930f5274c95f91387ee359e926
Jakob Antonsson <jakobant@gmail.com>
2017-11-27 23:15:51 +0000

    Beutify stuff

--- utils/screenshot.sh
+++ utils/screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+
 WAIT=${1:-20}
 sleep $WAIT
 export PRE=${2:-temp}
@@ -8,15 +8,17 @@ export DISPLAY=:0
 export DD=`date +%Y-%m-%d-%H%M%S`
 if [ -f /etc/fedora-release ]
 then
-	gnome-screenshot -f ${DD}_fedora.png
-	mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" ${DD}_fedora.png
+	gnome-screenshot -f ${DD}.png
+	mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" ${DD}.png
 	mv ${DD}_fedora.png $HOME/screenshots/$PRE
 elif [ -f /etc/redat-release ]
 then
-	gnome-screenshot -f ${DD}_redhat.png
-	mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" ${DD}_redhat.png
+	gnome-screenshot -f ${DD}.png
+	mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" ${DD}.png
 	mv ${DD}_redhat.png $HOME/screenshots/$PRE
 else
-	scrot -e 'mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" $f ; mv $f `echo \$HOME`/screenshots/`echo \$PRE`'
+	scrot ${DD}.png
+	mogrify -font Liberation-Sans -fill yellow -undercolor "#00000080" -pointsize 26 -annotate +30+30 "`date`" ${DD}.png
+	mv ${DD}_fedora.png $HOME/screenshots/$PRE
 fi
 
